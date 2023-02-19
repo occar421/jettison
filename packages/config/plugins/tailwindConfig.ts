decb8dc3b09966df8bd81807da11901ddd90dfec
MasuqaT <occar@hotmail.co.jp>
2023-02-19 19:44:16 +0900

    experimental tailwind config plugin

---
+++ packages/config/plugins/tailwindConfig.ts
@@ -0,0 +1,31 @@
+import { Config, JettisonRc } from "../index";
+
+// should be new module due to dependency on TailwindCSS
+export const collector: (pluginConfig: {
+  tailwindConfig: any;
+}) => JettisonRc["plugin"]["configCollectors"][number] =
+  (pluginConfig) => async () => {
+    return { tokens: pluginConfig["tailwindConfig"]["theme"] ?? {} };
+  };
+
+if (import.meta.vitest) {
+  const { it, expect } = import.meta.vitest;
+
+  it("empty config when empty tailwind config", async () => {
+    const result = await collector({
+      tailwindConfig: { theme: {} },
+    })();
+
+    expect(result).toStrictEqual({ tokens: {} } satisfies Config);
+  });
+
+  it("one token when 1 tailwind theme config", async () => {
+    const result = await collector({
+      tailwindConfig: { theme: { colors: { blue: "#1fb6ff" } } },
+    })();
+
+    expect(result).toStrictEqual({
+      tokens: { colors: { blue: "#1fb6ff" } },
+    } satisfies Config);
+  });
+}
