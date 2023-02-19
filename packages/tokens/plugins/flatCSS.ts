503eea2f564a661b8eeddcd1032c1ba87f8d6805
MasuqaT <occar@hotmail.co.jp>
2023-02-19 19:28:53 +0900

    accept plugin config

--- packages/tokens/plugins/flatCSS.ts
+++ packages/tokens/plugins/flatCSS.ts
@@ -1,18 +1,19 @@
 import { Config, JettisonRc } from "@jettison-system/config";
 
-export const render: JettisonRc["plugin"]["tokenRenderers"][number] = async (
-  config
-) => {
-  if (Object.keys(config.tokens).length === 0) {
-    return {};
-  }
+export const render: (pluginConfig: {
+  filePath: string;
+}) => JettisonRc["plugin"]["tokenRenderers"][number] =
+  (pluginConfig) => async (config) => {
+    if (Object.keys(config.tokens).length === 0) {
+      return {};
+    }
 
-  const variables = process(config.tokens).map(([k, v]) => `--${k}:${v};`);
+    const variables = process(config.tokens).map(([k, v]) => `--${k}:${v};`);
 
-  return {
-    "index.css": `:root{${variables.join("")}}`,
+    return {
+      [pluginConfig.filePath]: `:root{${variables.join("")}}`,
+    };
   };
-};
 
 function process(config: Config["tokens"]): [key: string, value: string][] {
   return Object.entries(config).flatMap(([k, v]) => {
@@ -29,20 +30,22 @@ function process(config: Config["tokens"]): [key: string, value: string][] {
 if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest;
 
+  const defaultRender = render({ filePath: "index.css" });
+
   it("empty object when empty config", async () => {
-    const result = await render({ tokens: {} });
+    const result = await defaultRender({ tokens: {} });
 
     expect(result).toStrictEqual({});
   });
 
   it("one CSS variable when 1 config", async () => {
-    const result = await render({ tokens: { main: "#000000" } });
+    const result = await defaultRender({ tokens: { main: "#000000" } });
 
     expect(result).toStrictEqual({ "index.css": `:root{--main:#000000;}` });
   });
 
   it("two CSS variable when 2 config", async () => {
-    const result = await render({
+    const result = await defaultRender({
       tokens: { main: "#000000", sub: "#FFFFFF" },
     });
 
@@ -52,7 +55,7 @@ if (import.meta.vitest) {
   });
 
   it("kebab-case named CSS variable when nested config", async () => {
-    const result = await render({
+    const result = await defaultRender({
       tokens: { main: { 0: "#000000" } },
     });
 
